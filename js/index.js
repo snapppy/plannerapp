be91620c8a1cf2338ddb76ecb6ead5435d08dbb8
snapppy <forever.spyder@gmail.com>
2017-02-09 20:57:22 -0700

    Arrays + Items Object + add item form
    Created an Item prototype, everytime you add an item it creates an
    object. A form now appears when you want to add an item. Will delete
    objects out of the array when checked off.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,32 @@
 var items = [];
 
+function Item(taskName, priorityLevel) {
+    this.taskName = taskName;
+    this.priorityLevel = priorityLevel;
+}
+
 function addItem() {
-    var j = 1,
-    i = 0,
+    var form = document.querySelector("#additemform");
+    form.style.display = "initial";
+    console.log("add item...");
+}
+
+function addNewItem() {
+    var i = 0,
+    taskname = document.getElementById("taskname").value,
+    prioritylevel = document.getElementById("prioritylevel").value,
+    item = new Item(taskname, prioritylevel),
     fromDom = document.querySelectorAll('.inputitem');
+    items.push(item);
 
-    items.push("Item " + j);
-    j++;
-
+    document.getElementById("task" + (items.length)).innerHTML = document.getElementById("taskname").value;
     for (i = 0; i < items.length; i++) {
         fromDom[i].style.display = "initial";
-    };
+    }
+
+    var form = document.querySelector("#additemform");
+    form.style.display = "none";
+    console.log("add NEW item...");
 }
 
 function deleteItem(index) {
